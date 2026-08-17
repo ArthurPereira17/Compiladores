72204f800a668c3953ae0e0f25df4868772aeab1
Arthur Nascimento Pereira <arthurnapereira@gmail.com>
2026-08-17 19:16:23 -0300

    teste de icones

--- interface_Equipe4/src/compilador/CompilerInterface.java
+++ interface_Equipe4/src/compilador/CompilerInterface.java
@@ -1,5 +1,3 @@
-package compilador;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
--- interface_Equipe4/src/compilador/IconFactory.java
+++ interface_Equipe4/src/compilador/IconFactory.java
@@ -1,5 +1,3 @@
-package compilador;
-
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
